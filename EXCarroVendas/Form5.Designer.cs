23bf665fc68f8e395da95ca059bb5bbb5f7d28dd
GabrielGaudi <gabriel.rosalvo@etec.sp.gov.br>
2024-05-21 10:50:04 -0300

    atualização

--- EXCarroVendas/Form5.Designer.cs
+++ EXCarroVendas/Form5.Designer.cs
@@ -33,6 +33,10 @@
             this.btnWhile = new System.Windows.Forms.Button();
             this.btnDoWhile = new System.Windows.Forms.Button();
             this.btnFor = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtNum
@@ -42,6 +46,7 @@
             this.txtNum.Name = "txtNum";
             this.txtNum.Size = new System.Drawing.Size(100, 29);
             this.txtNum.TabIndex = 0;
+            this.txtNum.TextChanged += new System.EventHandler(this.txtNum_TextChanged);
             // 
             // lblDigite
             // 
@@ -62,31 +67,82 @@
             this.btnWhile.TabIndex = 2;
             this.btnWhile.Text = "While";
             this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
             // 
             // btnDoWhile
             // 
             this.btnDoWhile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDoWhile.Location = new System.Drawing.Point(355, 344);
             this.btnDoWhile.Name = "btnDoWhile";
-            this.btnDoWhile.Size = new System.Drawing.Size(100, 35);
+            this.btnDoWhile.Size = new System.Drawing.Size(112, 35);
             this.btnDoWhile.TabIndex = 3;
             this.btnDoWhile.Text = "Do While";
             this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
             // 
             // btnFor
             // 
-            this.btnFor.Location = new System.Drawing.Point(608, 366);
+            this.btnFor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFor.Location = new System.Drawing.Point(581, 344);
             this.btnFor.Name = "btnFor";
-            this.btnFor.Size = new System.Drawing.Size(75, 23);
+            this.btnFor.Size = new System.Drawing.Size(75, 37);
             this.btnFor.TabIndex = 4;
-            this.btnFor.Text = "button1";
+            this.btnFor.Text = "For";
             this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(542, 184);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(100, 112);
+            this.txtResult.TabIndex = 5;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(259, 394);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(88, 44);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Voltar";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Fatoriais:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(480, 394);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(91, 44);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.txtResult);
             this.Controls.Add(this.btnFor);
             this.Controls.Add(this.btnDoWhile);
             this.Controls.Add(this.btnWhile);
@@ -106,5 +162,9 @@
         private System.Windows.Forms.Button btnWhile;
         private System.Windows.Forms.Button btnDoWhile;
         private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
--- EXCarroVendas/Form5.cs
+++ EXCarroVendas/Form5.cs
@@ -16,5 +16,77 @@ namespace EXCarroVendas
         {
             InitializeComponent();
         }
+
+        private void txtNum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            double i, num, numrsl, result;
+            num = Convert.ToDouble(txtNum.Text);
+            i = num - 1;
+            result = num * i;
+
+            while (i > 0) 
+            {
+                
+                txtResult.Text = string.Concat(txtResult.Text, "\r\n", result.ToString());
+                i--;
+                num = result;
+                result = result * i;
+                
+            }
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            double i, num, result;
+            num = Convert.ToDouble(txtNum.Text);
+            i = num - 1;
+            result = num * i;
+
+            for (i = num - 2; result > 0; i--)
+            {
+                num = result;
+                result = num * i;
+                txtResult.Text = string.Concat(txtResult.Text, "\r\n", result.ToString());
+             
+            }
+
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            double i, num, result;
+            num = Convert.ToDouble(txtNum.Text);
+            i = num - 1;
+            result = num * i;
+
+            do
+            {
+                
+                txtResult.Text = string.Concat(txtResult.Text, "\r\n", result.ToString());
+                i--;
+                num = result;
+                result = result * i;
+
+            } while (i > 0);
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            PgMenu tela = new PgMenu();
+            tela.Show();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNum.Clear();
+            txtResult.Clear();
+            txtNum.Focus();
+        }
     }
 }
